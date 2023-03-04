865bab8fe12cf996b2c1f0cc90444c319c3ea373
Kentucky SATO <kentucky.sato@gmail.com>
2023-03-04 20:24:37 +0100

    feat: add input range for length of password
    Generate password each time user change the length of pwd

--- popup.js
+++ popup.js
@@ -1,23 +1,39 @@
-// Génère un mot de passe aléatoire sécurisé
+/**
+ * Generate Password
+ *
+ * @file popup.js
+ * @description This file contains the code for the popup page
+ * @author Kentucky SATO
+ */
 function generatePassword() {
-    // Caractères possibles pour le mot de passe
+    // Possibles characters for the password
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-    let passwordLength = 20; // Longueur du mot de passe
+
+    let passwordLength = document.getElementById("lenght_password").value; // Length of the password
     let password = "";
 
-    // Boucle pour générer chaque caractère du mot de passe
+    // Loop to generate the password
     for(let i = 0; i < passwordLength; i++) {
         let randomNumber = Math.floor(Math.random() * characters.length);
         password += characters.substring(randomNumber, randomNumber + 1);
     }
 
-    // Affiche le mot de passe généré dans le champ de texte
+    // Display the password in the text box
     document.getElementById("password").value = password;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Code à exécuter une fois que la page est entièrement chargée
-    // Ajoutez ici votre code pour ajouter des écouteurs d'événements
-    // Appelle la fonction generatePassword() lorsqu'on clique sur le bouton "Générer"
+// Execute the function when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    const valueRange = document.querySelector("#value")
+    const inputRange = document.querySelector("#lenght_password")
+    valueRange.textContent = inputRange.value
+    inputRange.addEventListener("input", (event) => {
+        valueRange.textContent = event.target.value
+    })
+
+    // Call the function generatePassword() when we click on the "Generate" button
     document.getElementById("generate").addEventListener("click", generatePassword);
+
+    // Call the function generatePassword() when we change the value of the range
+    document.getElementById("lenght_password").addEventListener("input", generatePassword);
 });
